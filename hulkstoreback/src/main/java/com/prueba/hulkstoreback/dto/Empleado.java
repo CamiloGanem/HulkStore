ea36d633b34f64ed562c1d3dbcf0c648bd42660f
Camilo Ganem <camiloganemortega@gmail.com>
2023-05-04 21:41:23 -0500

    Implementiacion de atributos y configuracion de jpa para la clase Empleado y Producto

--- hulkstoreback/src/main/java/com/prueba/hulkstoreback/dto/Empleado.java
+++ hulkstoreback/src/main/java/com/prueba/hulkstoreback/dto/Empleado.java
@@ -1,2 +1,23 @@
-package com.prueba.hulkstoreback.dto;public class Empleado {
+package com.prueba.hulkstoreback.dto;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "hs_empleados")
+public class Empleado {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id_empleado;
+    private String iden_empleado;
+    private String nombres_empleado;
+    private String apellidos_empleados;
+    private Date fecha_nac_empleado;
 }
--- hulkstoreback/src/main/java/com/prueba/hulkstoreback/dto/Producto.java
+++ hulkstoreback/src/main/java/com/prueba/hulkstoreback/dto/Producto.java
@@ -1,2 +1,22 @@
-package com.prueba.hulkstoreback.dto;public class Producto {
+package com.prueba.hulkstoreback.dto;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "hs_productos")
+public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id_producto;
+    private String codigo_producto;
+    private String nombre_producto;
+    private String descripcion_producto;
+    private Double precio_producto;
+    private int cantidad_producto;
 }
