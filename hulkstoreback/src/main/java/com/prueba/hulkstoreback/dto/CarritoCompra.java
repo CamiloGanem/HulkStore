27f783c6636af1b7007e90829fc9ae8e26d96859
Camilo Ganem <camiloganemortega@gmail.com>
2023-05-05 19:31:42 -0500

    Se crea componente para login

--- hulkstoreback/src/main/java/com/prueba/hulkstoreback/dto/CarritoCompra.java
+++ hulkstoreback/src/main/java/com/prueba/hulkstoreback/dto/CarritoCompra.java
@@ -1,2 +1,27 @@
-package com.prueba.hulkstoreback.dto;public class CarritoCompra {
+package com.prueba.hulkstoreback.dto;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "hs_carrito_compras")
+public class CarritoCompra {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id_carrito_compra;
+
+    @ManyToOne
+    @JoinColumn(name = "id_producto", foreignKey = @ForeignKey(name = "id_producto", value = ConstraintMode.CONSTRAINT))
+    private Producto producto;
+
+    @OneToOne
+    @JoinColumn(name = "id_empleado", foreignKey = @ForeignKey(name = "id_empleado", value = ConstraintMode.CONSTRAINT))
+    private Empleado empleado;
+
+    private int cantidad_carrito;
 }
