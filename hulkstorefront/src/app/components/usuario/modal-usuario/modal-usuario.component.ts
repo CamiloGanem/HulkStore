ae45d5fa77a3e8cd760587d803d260958d9fb075
Camilo Ganem <camiloganemortega@gmail.com>
2023-05-07 23:19:31 -0500

    Diseño y consumo de servicio para creacion de usuario mediante el componente modal-usuario

--- hulkstorefront/src/app/components/usuario/modal-usuario/modal-usuario.component.ts
+++ hulkstorefront/src/app/components/usuario/modal-usuario/modal-usuario.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "../../../service/auth/auth.service";
 
 @Component({
   selector: 'app-modal-usuario',
   templateUrl: './modal-usuario.component.html',
   styleUrls: ['./modal-usuario.component.css']
 })
-export class ModalUsuarioComponent {
+export class ModalUsuarioComponent implements OnInit {
+
+  form!: FormGroup;
+  constructor(public dialogRef: MatDialogRef<ModalUsuarioComponent>,
+              @Inject(MAT_DIALOG_DATA) public data: any, private fm: FormBuilder,
+              private auth: AuthService) {
+
+    this.iniciarFormulario();
+  }
+
+  ngOnInit(): void {}
+
+  onCancelClick(): void {
+    this.dialogRef.close();
+  }
+
+  guardar(): void {
+    console.log(this.form.value);
+    const usuario:any =
+      {
+        firtName: this.form.value.primer_nombre,
+        lastName: this.form.value.primer_apellido,
+        email: this.form.value.email,
+        password:this.form.value.password,
+        empleado:{
+          id_empleado:"",
+          iden_empleado: this.form.value.iden_empleado,
+          primer_nom_empleado: this.form.value.primer_nombre,
+          segundo_nom_empleado: this.form.value.segundo_nombre,
+          primer_apell_empleado: this.form.value.primer_apellido,
+          segundo_apell_empleado: this.form.value.segundo_apellido,
+          fecha_nac_empleado: this.form.value.fecha_nac
+        }
+    }
+
+    this.auth.registarUsuario(usuario).subscribe( res => {
+      console.log(res)
+      alert(res.mensaje);
+    }, error => {
+      console.log(error)
+      alert(error.error.text)
+    });
+  }
+
+  iniciarFormulario():void{
+    this.form = this.fm.group(
+      {
+        primer_nombre: ['', Validators.required],
+        segundo_nombre: [''],
+        primer_apellido:['', Validators.required],
+        segundo_apellido:[''],
+        iden_empleado:['', Validators.required],
+        fecha_nac: ['', Validators.required],
+        email: ['', Validators.required],
+        password:['', Validators.required]
+      }
+    )
+  }
 
 }
